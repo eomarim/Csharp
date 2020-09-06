b68081292409482475d8a35d7a19b2b0a75beb5c
Eduardo Marim <eduardomarim@Eduardos-MacBook-Pro.local>
2020-09-06 10:32:55 -0300

    Adicionando RefInOut

---
+++ RefInOut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefInOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
