61e9a15f5b20e87b88e7c300db50e3f00d9da45b
eomarim <e.oliveiramarim@gmail.com>
2020-09-06 11:20:21 -0300

    Boxing e Unboxing

--- RefInOut/Program.cs
+++ RefInOut/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using System.Net.NetworkInformation;
+using System;
 
 namespace RefInOut
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Ref precisa que o parametro informado tenha uma atribuicao;
+            int x = 5;
+            ValorPorReferencia_Ref(ref x);
+            Console.WriteLine($"Valor de X passado por referencia(Ref) do Metodo Valor Por Referencia: {x}" );
+
+            int y; //out - nao precisa ser inicializado. E util quando queremos que o metodo retorne mais de um valor
+            ValorPorReferencia_Out(out y);
+            Console.WriteLine($"Valor de y passado por referencia(Out) do Metodo Valor Por Referencia: {y}" );
+        }
+
+        public static void ValorPorReferencia_Ref(ref int valor){
+            valor = 10;
+        }
+
+        public static void ValorPorReferencia_Out(out int valor){
+            valor = 20;
         }
     }
 }
