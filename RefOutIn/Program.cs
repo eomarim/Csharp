8ab0d8cd62fb5e7c16b6bb90d8e3e528ddb8004c
System Administrator <root@Eduardos-MacBook-Pro.local>
2020-09-06 10:08:27 -0300

    refOutIn

---
+++ RefOutIn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefOutIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
