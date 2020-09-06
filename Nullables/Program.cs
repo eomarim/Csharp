eb4894d09898c7a5b109d1cb3366320050d6952d
Eduardo Marim <eduardomarim@Eduardos-MacBook-Pro.local>
2020-09-06 08:58:45 -0300

    Criando Params

---
+++ Nullables/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Nullables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Terminal t = new Terminal(0, "IT000320", "POS");
+
+            Terminal? y = t;
+
+            //p e w irai aceitar valores nulos.
+            Nullable<Terminal> p = null; //Pode-se utilizar dessa forma ou 
+            Terminal? w = null; //dessa forma
+
+            Terminal? f = w ?? y;         
+
+            Console.WriteLine("Valor de T: " + t);
+            System.Console.WriteLine("Valor de Y: " + y.Value);
+            Console.WriteLine("Valor de P: " + p);
+            System.Console.WriteLine("Valor de W:" + w.GetValueOrDefault());
+
+            System.Console.WriteLine("Valor de F: " + f);
+            
+            if(y.HasValue)
+                System.Console.WriteLine("Y Possui valor");
+            else
+                System.Console.WriteLine("Y Nao possui valor");
+            
+            if(w.HasValue)
+                System.Console.WriteLine("W possui valor");
+            else
+                System.Console.WriteLine("W nao possui valor");
+
+            int? inteiro = null;
+            int? inteiro2 = null;
+
+            int v = inteiro2 ?? 30;
+            inteiro = v;
+
+            System.Console.WriteLine($" valor de inteiro e: {inteiro}");
+            System.Console.WriteLine($"Inteiro possui valor: {inteiro.HasValue}");
+            System.Console.WriteLine($"Valor de Inteiro:{inteiro.GetValueOrDefault()}");
+        }
+    }
+
+    public struct Terminal {
+        public int CodTerminal { get; set; }
+        public string NomeTerminal { get; set; }
+
+        public string TipoTerminal { get; set; }
+
+        public Terminal(int codTerminal, string nomeTerminal, string tipoTerminal){
+            this.CodTerminal = codTerminal;
+            this.NomeTerminal = nomeTerminal;
+            this.TipoTerminal = tipoTerminal;
+        }
+        //Nao aceita sobrecarga de construtores.
+        /*public Terminal(int codTerminal, string nomeTerminal, string tipoTerminal):this(codTerminal, nomeTerminal){
+            this.TipoTerminal = tipoTerminal; 
+        }*/
+
+        public override string ToString()
+        {
+            return $"Cod: {CodTerminal}, Terminal: {NomeTerminal}, Tipo: {TipoTerminal}";
+        } 
+
+    }
+}
---
+++ Params/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System;
+
+namespace Params
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           Console.WriteLine(      Soma(10, 55.3, 20.6, 10.25).ToString("F2", CultureInfo.InvariantCulture)       );
+        }
+                                //Informe params e passe somente os valores
+        public static double Soma(params double[] numeros){
+            double resultado = 0.00;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                resultado+= numeros[i];
+            }
+            return resultado;
+        }
+    }
+}
