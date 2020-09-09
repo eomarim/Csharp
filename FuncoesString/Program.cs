5f800717a4c1a12056a7e2ecee639958be103da6
Eduardo Marim <e.oliveiramarim@gmail.com>
2020-09-09 19:15:48 -0300

    novos exemplos

---
+++ FuncoesString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FuncoesString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
